6340627b75c2197b98c7c5a30fd30db08124cbf8
Izabelly Labegalini <bellylabegalini@hotmail.com>
2025-04-29 10:37:40 +0000

    Tired to try it

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -73,14 +73,12 @@ WSGI_APPLICATION = "web_django.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "djongo",
         "NAME": "octofit_db",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
